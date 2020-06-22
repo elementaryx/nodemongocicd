1ab88d6db6a0ccf2b38a4781cae2d659ac30f909
BIBIN <itsmebibin333@gmail.com>
2020-06-22 17:57:28 +0530

    Provider TF update

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
     region = "us-west-2"
-    access_key = "AKIAXMHK63NYRQXY5GNB"
-    secret_key = "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"
+    access_key = env.AWS_ACCESS_KEY_ID
+    secret_key = env.AWS_SECRET_ACCESS_KEY
   
 }
